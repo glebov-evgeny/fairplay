6ada5dea7dc671112493fc6c45516983f06aaf05
Евгений Глебов <egoji@mail.ru>
2024-01-09 10:57:06 +0300

    start

---
+++ components/s-footer/s-footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer class="s-footer">
+    <div class="s-footer__container l-wide">{{ getYear }}. И точка.</div>
+  </footer>
+</template>
+
+<script setup>
+const getYear = computed(() => {
+  const currentYear = new Date();
+  return currentYear.getFullYear();
+});
+</script>
+
+<style lang="scss">
+@import './s-footer.scss';
+</style>
