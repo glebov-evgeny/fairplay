0bc54dd206a43eb35d78c349df247966da9b0d46
Евгений Глебов <egoji@mail.ru>
2024-01-11 15:58:52 +0300

    update lk

--- components/s-lk/s-lk.vue
+++ components/s-lk/s-lk.vue
@@ -1,11 +1,41 @@
+<!-- eslint-disable vue/no-parsing-error -->
 <template>
   <section class="s-lk">
     <div class="s-lk__container l-wide">
-      <h1 class="s-lk__title">Личный кабинет</h1>
+      <h1 class="s-lk__title s-title">Личный кабинет</h1>
+      <div class="s-lk__box">
+        <div class="s-lk__user-wrapper">
+          <div class="s-lk__user" :style="{ backgroundColor: `${userColor}` }">
+            <div class="s-lk__user-item">
+              <p class="s-lk__user-item-label"><b>Почта:</b></p>
+              <div class="s-lk__user-item-wrapper">
+                <p class="s-lk__user-item-label">
+                  {{ currentUser.email }}
+                </p>
+              </div>
+            </div>
+            <!-- <div class="s-lk__user-item">
+              <p class="s-lk__user-item-label">id:</p>
+              <p class="s-lk__user-item-label _bold">{{ currentUser.uid }}</p>
+            </div> -->
+            <div class="s-lk__user-item">
+              <p class="s-lk__user-item-label"><b>Любимый цвет:</b></p>
+              <div class="s-lk__user-item-wrapper">
+                <input v-model="userColor" type="color" class="s-lk__user-item-color" />
+              </div>
+            </div>
+          </div>
+          <!-- <button class="s-lk__user-button">сохранить</button> -->
+        </div>
+      </div>
     </div>
   </section>
 </template>
-
+<script setup>
+import { useUserStore } from '~/store/user';
+const currentUser = useUserStore();
+const userColor = ref('#FFFFFF');
+</script>
 <style lang="scss">
 @import './s-lk.scss';
 </style>
