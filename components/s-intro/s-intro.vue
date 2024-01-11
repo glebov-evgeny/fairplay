22e044dadbeee9357a125877980cc42ed95d956b
Евгений Глебов <egoji@mail.ru>
2024-01-11 14:14:26 +0300

    add cookie popup

--- components/s-intro/s-intro.vue
+++ components/s-intro/s-intro.vue
@@ -5,7 +5,7 @@
         <h1 class="s-intro__title s-title">Кря-кря!</h1>
         <p class="s-intro__description s-description">
           Добро пожаловать на&nbsp;сайт, где каждый может сделать кря. Ведь кря&nbsp;&mdash; это нужное дело. Кря звучит
-          гордо, кря это вызов. Кря это свобода и&nbsp;любовь, это квинтэссенция экзистенциального существования.
+          гордо, кря это вызов. Кря это свобода и&nbsp;любовь, кря - это квинтэссенция экзистенциального существования.
         </p>
         <p class="s-intro__description s-description">
           Даже если прямо сейчас вы&nbsp;не&nbsp;хотите делать кря&nbsp;&mdash; кря всегда можно сделать позже.
@@ -15,7 +15,7 @@
       <div class="s-intro__aside">
         <h2 class="s-intro__aside-title s-title">Эти новости&nbsp;&mdash; уже не&nbsp;новости.</h2>
         <p class="s-intro__aside-description s-description">
-          Всё, что произошло за&nbsp;полярным кругом, должно остаться за&nbsp;полярным кругом.
+          Всё, что случилось за&nbsp;полярным кругом, должно остаться за&nbsp;полярным кругом.
         </p>
         <p class="s-intro__aside-description s-description">People are always asking me if I know Tyler Durden.</p>
       </div>
